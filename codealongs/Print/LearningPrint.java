9b85755963e216a75f6f1e0a0d2aa5b8aac70cf4
tomanstensen <tom.anstensen@iths.se>
2023-09-11 09:00:33 +0200

    print exempel

---
+++ codealongs/Print/LearningPrint.java
@@ -0,0 +1,19 @@
+package codealongs.Print;
+
+public class LearningPrint {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!");
+        System.out.println( '\u00E4');
+
+        int cars = 8;
+        System.out.println(cars);
+
+        System.out.println("Hello World!");
+        System.out.print("Hej");
+        System.out.println("Hej!");
+
+        System.out.printf("hello world?");
+
+
+    }
+}
