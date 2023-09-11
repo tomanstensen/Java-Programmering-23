4ffe37eb95ae82c650295b2be352a15b22056a37
tomanstensen <tom.anstensen@iths.se>
2023-09-11 09:03:13 +0200

    scanner exempel

---
+++ codealongs/Scanner/LearningScanner.java
@@ -0,0 +1,27 @@
+package codealongs.Scanner;
+
+import java.util.Scanner;
+
+public class LearningScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int myNumber;
+        System.out.println("Please input a number: ");
+        myNumber = scanner.nextInt();
+
+        System.out.println("Input the avarage salary: ");
+        double avgSalary = scanner.nextDouble();
+
+        System.out.println("Please enter a message: ");
+
+        String message = scanner.nextLine();
+
+        System.out.println("Input number: " + myNumber);
+        System.out.println("The average salary is " + avgSalary);
+        System.out.println("The message is: " + message);
+
+        scanner.close();
+
+    }
+}
