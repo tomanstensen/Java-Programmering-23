7825d6f57dd60f010a4b22d14bfcc391db020b71
tomanstensen <tom.anstensen@iths.se>
2023-09-20 11:47:02 +0200

    enkel multiplikationstabell

---
+++ eget/multipl.java
@@ -0,0 +1,11 @@
+public class multipl {
+    public static void main(String[] args){
+      int nummer = 6;
+      
+        int tabell = 1;
+      while(tabell <= 10){
+        System.out.println(nummer*tabell);
+        tabell++;
+      }
+    }
+}
