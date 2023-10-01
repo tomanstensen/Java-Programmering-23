4c93c823f94b16777858bbd47ae0680de9037012
tomanstensen <tom.anstensen@iths.se>
2023-10-01 20:46:18 +0200

    testat lite grejer

--- eget/continueLoop.java
+++ eget/continueLoop.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class continueLoop {
     public static void main(String[] args) {
-        System.out.print( "Would you like to continue? Y/N: ");
+        Scanner answer = new Scanner(System.in);
+        String keepGoing = "Y";
+        
+        while(keepGoing == "Y" || keepGoing == "y"){
+            System.out.print( "Would you like to continue? Y/N: ");
+            keepGoing = answer.next();
+        }
+        
     }
 }
--- eget/testThings.java
+++ eget/testThings.java
@@ -1,4 +1,9 @@
 public class testThings {
     public static void main(String[] args){
      }
+    class Person{
+        String name;
+        int age;
+        char gender;
+    }
 }
